797272faf4d41d724181260da71a7411f0e97c84
Keshav Agrawal <keshavagrawal1123@gmail.com>
2024-08-22 07:07:26 +0530

    some styling fixes

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+// Variables -> container for holding values
+// let pi = 3.14;
+// console.log(pi);
+// let a = 10;
+// let b = 10;
+// let sum = a + b;
+// console.log(sum);
+
+// const pi = 3.14;
+// pi = 15;
+
+var varKey;
+a = 10;
+var b = 10;
+let c = 10;
+const d = 10;
+let name = "john";
+let firstName = "John";
+let lastName = "Doe";
+console.log(firstName + " " + lastName);
+let x = "5" + 2 + 3;
+console.log(x);
+
+console.log(varKey);
+{
+  let undec = "Name";
+}
+console.log(undec);
+
+const fruits = ["Apple", "Mango", "Orange"];
+fruits.push("Pineapple");
